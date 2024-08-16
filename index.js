35e898201a974f307f689543e4da65a7b7f9e46d
nadhirbk <nadhir.benkhaled@gmail.com>
2024-08-16 14:58:27 +0200

    Ajout du fichier recepes.js et du premier filtre de recherche

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 import { createCard } from "./scripts/card.js";
+import { recepes } from "./scripts/recipes.js";
+
+let cardsContainer;
+const allCards = [];
+
+// array of selected IDs
+let filteredCards = [];
+
+const renderCards = () => {
+  console.log(allCards);
+  allCards.forEach((c) => c.element.remove());
+  filteredCards.forEach((id) => {
+    const current = allCards.find((card) => card.id === id);
+    cardsContainer.appendChild(current.element);
+  });
+};
 
 window.onload = () => {
-  const cards = document.getElementById("recepes");
-  for (let i = 0; i < 5; i++) {
-    const card = createCard();
-    cards.appendChild(card);
-  }
+  cardsContainer = document.getElementById("recepes");
+  recepes.forEach((recepe) => {
+    const card = createCard(recepe);
+    allCards.push({
+      id: recepe.id,
+      recepe: recepe,
+      element: card,
+    });
+    filteredCards.push(recepe.id);
+  });
+
+  renderCards();
+
+  const input = document.getElementById("input-search");
+  input.oninput = (ev) => {
+    const search = ev.target.value.toLowerCase();
+    if (search.length > 3) {
+      filteredCards = allCards
+        .filter((card) => {
+          const recepe = card.recepe;
+          if (recepe.name.toLowerCase().includes(search)) return true;
+          if (recepe.description.toLowerCase().includes(search)) return true;
+          return recepe.ingredients.find((ing) =>
+            ing.ingredient.includes(search)
+          );
+        })
+        .map((item) => item.id);
+      renderCards();
+    } else {
+      filteredCards = allCards.map((c) => c.id);
+      renderCards();
+    }
+  };
 };
--- scripts/card.js
+++ scripts/card.js
@@ -1,5 +1,11 @@
-export const createCard = () => {
+export const createCard = (recepe) => {
   const template = document.getElementById("recepe").content;
   const card = template.cloneNode(true);
-  return card;
+  const img = card.querySelector(".recepe-img");
+  img.src = "./assets/recepes/" + recepe.image;
+  const title = card.querySelector(".recepe-title");
+  title.textContent = recepe.name;
+  const description = card.querySelector(".description");
+  description.textContent = recepe.description;
+  return card.querySelector(".card");
 };
